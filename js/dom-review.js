8b6bfa525beda778ffbfe8356b8234bf5f380fd4
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-01-01 14:53:21 -0600

    dom-review part 4

--- js/dom-review.js
+++ js/dom-review.js
@@ -12,29 +12,14 @@ $('h1').click(
 
 //TODO: Write a function that causes the runner div to move to the right side of the screen when the mouse moves over it
 
-$('.runner').on('hover', function () {
-    $(this)
-        .appendTo( $('.right-side') )
-        .unbind('hover');
+
+$(document).ready(function(){
+    $(".runner").hover(function(){
+        $(".runner").appendTo("#right-side");
+        $(this).hide();
+    });
 });
 
-// $('.runner').mouseenter(function(){
-//     $(this).addClass('hover');
-// });
-
-// $('.runner').mouseleave(function(){
-//     $(this).removeClass('hover');
-// });
-// $('.runner').on("mousemove", function() {
-//         $(this).addEventListener('touchmove', 'right')
-//     },
-// );
-
-// $('.runner').on("mousemove", function() {
-//     var vW = $(window).width();
-//     let mW = ((((event.pageX)*100)/vW).toFixed(3))-50;
-//     $("#red").css('transform', 'translateX(' +(-mW/2)+ '%)');
-// });
 //TODO: Write a function to take the value from the input and append the value onto the ul with the id of add-to-me.
 
 
